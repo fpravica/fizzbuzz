d59c1d492e10c404f1ca94e8e11c9354df4c098f
Filip Pravica <fpravica@gmail.com>
2015-07-29 02:40:41 +0200

    Added comments

--- src/main/java/com/prottone/fizzbuzz/Buzzers.java
+++ src/main/java/com/prottone/fizzbuzz/Buzzers.java
@@ -5,24 +5,36 @@ package com.prottone.fizzbuzz;
  */
 public class Buzzers {
 
+    /**
+     * Returns "Fizz" if index divisible by 3
+     */
     public static final Buzzer FIZZ = new Buzzer() {
         public String execute(int index) {
             return (index % 3 == 0) ? "Fizz" : "";
         }
     };
 
+    /**
+     * Returns "Buzz" if index divisible by 5
+     */
     public static final Buzzer BUZZ = new Buzzer() {
         public String execute(int index) {
             return (index % 5 == 0) ? "Buzz" : "";
         }
     };
 
+    /**
+     * Returns "Woof" if index divisible by 7
+     */
     public static final Buzzer WOOF = new Buzzer() {
         public String execute(int index) {
             return (index % 7 == 0) ? "Woof" : "";
         }
     };
 
+    /**
+     *  Returns "Foo" for 11, 22, 33, 44, 55...
+     */
     public static final Buzzer FOO = new Buzzer() {
         public String execute(int index) {
             return (index % 11 == 0) ? "Foo" : "";
